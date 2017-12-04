d9c16f590f92de4da4533a8fdf6b2d8ea39451bc
Takamichi-tsutsumi <takamichi_tsutsumi@waku-2.com>
2017-12-05 02:31:32 +0900

    graphql

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 import restify from 'restify'
+import { graphql, buildSchema } from 'graphql'
 
-const respond = (req, res, next) => {
-  res.send({ name: req.params.name })
+const schema = buildSchema(`
+type Query {
+  hello: String
+}
+`);
+
+const root = { hello: () => 'Hello world!' };
+
+const respond = async (req, res, next) => {
+  const response = await graphql(schema, '{ hello }', root)
+  
+  res.send(response)
   next()
 }
 
+
 const server = restify.createServer()
-server.get('/hello/:name', respond)
-server.head('/hello/:name', respond)
+server.get('/graphql', respond)
+server.head('/graphql', respond)
 
 server.listen(8080, function() {
   console.log('%s listening at %s', server.name, server.url)
